2222e05d4efd2694f617e76ed49e4e25c8cdce3f
sukunya2809 <sukunya.sam28@gmail.com>
2017-11-19 14:50:57 +0700

    Testing upload to github

---
+++ src/oop_lab/s357411110066/rmutsv/com/helloworld.java
@@ -0,0 +1,10 @@
+package oop_lab.s357411110066.rmutsv.com;
+
+public class helloworld {
+
+    // create main method
+    public static void main (String [] args){
+
+        System.out.println("Hello World, I'am Sam");
+}//main
+        }
