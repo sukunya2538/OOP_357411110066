a9d64596b4b1b8eb2ec0668adadea355549f819e
sukunya2809 <sukunya.sam28@gmail.com>
2017-11-19 15:21:31 +0700

    Edit information

--- src/oop_lab/s357411110066/rmutsv/com/helloworld.java
+++ src/oop_lab/s357411110066/rmutsv/com/helloworld.java
@@ -7,8 +7,8 @@ public class helloworld {
 
         System.out.println("Hello World, I'am Sam");
         System.out.println("sukunya dittaruk");
-        System.out.println("Informaytion suystem:IS");
-        System.out.println("MT:rmutsv");
+        System.out.println("Informaytion suystem:IS,MT Rmutsv");
+
 
 
 }//main
