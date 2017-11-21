725a34e5237e269145b0620bf22759cb6caf3264
sam28092538 <sam28092538@gmail.com>
2017-11-21 11:14:00 +0700

    Test Variable

---
+++ src/oop_lab2/TestVariable.java
@@ -0,0 +1,68 @@
+package oop_lab2;
+
+public class TestVariable {
+   public static void main(String[]args){
+ //      Integer
+       int i;
+       i=10;
+       System.out.println ("i="+i);
+       int j=20;
+       System.out.println("j="+j);
+       int x =i + j;
+       System.out.println("x="+x);
+// Double
+       double v =10.10;
+       System.out.println("v="+v);
+       x = (int) v;
+       System.out.println("x="+x);
+//       Char
+       char c = 'T';
+       System.out.println("c="+c);
+//       String
+        String s = "hello,world";
+        System.out.println(s);
+        String name = "Sam";
+        System.out.println(s+" "+name);
+
+        String msg = s.concat(name);
+        System.out.println(msg);
+        System.out.println(msg.length());
+
+        System.out.println(msg.toUpperCase());
+        System.out.println(msg.toLowerCase());
+
+       int number = 10;
+       int m = ++number;
+       System.out.println(number);
+       System.out.println(m);
+
+       m+=1; // m=m+1;
+       System.out.println(m);
+       m+=10;
+       System.out.println(m);
+       m-=5;
+       System.out.println(m);
+       m*=10;
+       System.out.println(m);
+
+ //    Boolean
+       boolean b = true;
+       boolean u = false;
+       System.out.println(b);
+       b = false;
+       System.out.println(b);
+
+
+
+
+
+
+
+
+
+
+     }//main
+
+
+
+}// class
