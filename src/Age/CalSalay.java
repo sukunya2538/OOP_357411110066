f58fc8d8a4213702ff115f8fc4ccf425ea2e58c7
sam28092538 <sam28092538@gmail.com>
2018-01-19 17:35:17 +0700

    CalSalayอันใหม่

--- src/Age/CalSalay.java
+++ src/Age/CalSalay.java
@@ -4,31 +4,55 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class CalSalay {
-    static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) throws IOException{
-        String id = getID();
-        String name = getName();
-        String salay = getSalay();
-        String overTime = getOverTime();
+    public static void main(String[] args) {
+        String getName;
+        int getID;
+        int getSalary = 0;
+        int net = 0;
+        int salarynet = 0;
+        int getOverTime = 0;
 
-        System.out.println(id+"\n"+name+"\n"+salay+"\n"+overTime);
-        System.out.println(getID()+"\n"+getName()+"\n"+getSalay()+"\n"+getOverTime());
+        Scanner oj = new Scanner(System.in);
+        System.out.print("\nรหัสพนักงาน: ");
+        getID = oj.nextInt();
 
-    }//getID
-    public static String getID()throws IOException {
-        System.out.print("Enter your id: ");
-        return scanner.nextLine();
-    }//getName
-    public static String getName()throws IOException{
-        System.out.print("Enter your name: ");
-        return scanner.nextLine();
-    }//getSalay
-    public static String getSalay()throws IOException{
-        System.out.print("Enter your  and salay: ");
-        return scanner.nextLine();
-    }//getOverTime
-    public static String getOverTime()throws IOException{
-        System.out.print("Enter your overTime: ");
-        return scanner.nextLine();
-    }//main
-}//class
+        Scanner Keyboard = new Scanner(System.in);
+        System.out.print("\nชื่อพนักงาน: ");
+        getName = Keyboard.nextLine();
+
+        System.out.print("\nเงินเดือนพนักงาน: ");
+        getSalary = oj.nextInt();
+
+        System.out.print("\nค่าล่วงเวลา: ");
+        getOverTime = oj.nextInt();
+
+        if (getSalary+getOverTime >= 100000) {
+            net = (getSalary + getOverTime);
+            System.out.println("\nเสียภาษี: 10%");
+            System.out.println("\nเงินเดือนสุทธิ: " + net);
+            System.out.println("\nภาษี:" + net*10/100);
+        } else if ((getSalary+getOverTime >= 70000)&&(getSalary+getSalary <= 99999)) {
+            net = (getSalary + getOverTime);
+            System.out.println("\nเสียภาษี: 7%");
+            System.out.println("\nเงินเดือนสุทธิ: " + net);
+            System.out.println("\nภาษี:" + net*7/100);
+        } else if ((getSalary+getOverTime >= 50000)&&(getSalary+getSalary <= 69999)) {
+            net = (getSalary + getOverTime);
+            System.out.println("\nเสียภาษี: 5%");
+            System.out.println("\nเงินเดือนสุทธิ: " + net);
+            System.out.println("\nภาษี:" + net*5/100);
+        }else if ((getSalary+getOverTime >= 30000)&&(getSalary+getOverTime <= 49999)) {
+            net = (getSalary + getOverTime);
+            System.out.println("\nเสียภาษี: 3%");
+            System.out.println("\nเงินเดือนสุทธิ: " + net);
+            System.out.println("\nภาษีที่ต้องเสีย:" + net*3/100);
+
+        }
+        else if (getSalary+getOverTime < 29999){
+            net = (getSalary + getOverTime);
+            System.out.println("\nเสียภาษี:1%");
+            System.out.println("\nเงินเดือนสุทธิ: "+ net);
+            System.out.println("\nภาษีที่ต้องเสีย:" +net*1/100);
+        }
+    }
+}
