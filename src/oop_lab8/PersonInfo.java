20498d6afb0076b65bd784fb0b6a295fe152e33f
sam28092538 <sam28092538@gmail.com>
2018-01-30 09:57:58 +0700

    PersonInfo

---
+++ src/oop_lab8/PersonInfo.java
@@ -0,0 +1,20 @@
+package oop_lab8;
+
+public class PersonInfo {
+    public static void main(String[] args) {
+        Student student = new Student("1809700249043",
+                " sukunya",
+                "Gril",
+                23,
+                "357411110066",
+                "IS :Management Technoloay");
+          System.out.println(student.toString());
+          System.out.println(student.getName());
+
+
+
+
+
+
+    }//main
+}//class
