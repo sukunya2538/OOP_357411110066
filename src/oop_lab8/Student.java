9c584edc4dceb4f15f666d977b68630501daa4ce
sam28092538 <sam28092538@gmail.com>
2018-01-30 09:36:51 +0700

    Inheritance:student Person

---
+++ src/oop_lab8/Student.java
@@ -0,0 +1,40 @@
+package oop_lab8;
+//Person is super-class
+//Student is sub-class
+
+public class Student extends Person {
+    private String studentID;
+    private String major;
+
+    public Student(String personID, String name, String gender, int age, String studentID, String major) {
+        //การเรียกใช้งาน constructor ของ super class
+        super(personID, name, gender, age);
+        this.studentID = studentID;
+        this.major = major;
+    }
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "studentID='" + studentID + '\'' +
+                    ", major='" + major + '\'' +
+                    "} " + super.toString();
+    }
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    }
+
+
